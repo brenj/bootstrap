a71532b6f5395d903288731694372d40e2dea26b
brenj <brenj@mailbox.org>
2017-08-17 22:13:39 -0500

    Link tern-config file

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -24,6 +24,7 @@ ln -sf dotfiles/.bashrc .bashrc
 ln -sf dotfiles/.bash_aliases .bash_aliases
 ln -sf dotfiles/.bash_profile .bash_profile
 ln -sf dotfiles/.gvimrc .gvimrc
+ln -sf dotfiles/.tern-config .tern-config
 ln -sf dotfiles/.tmux.conf .tmux.conf
 ln -sf dotfiles/.vimrc .vimrc
 ln -sf dotfiles/.vim .vim
