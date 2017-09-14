ba82bf495c5c965559e56518e1f828279cca397f
brenj <brenj@mailbox.org>
2017-09-14 15:37:14 -0500

    Add cask repo and install xquartz
    Will make more dynamic if more cask packages are installed.

--- macOS/provision.sh
+++ macOS/provision.sh
@@ -6,7 +6,11 @@ PACKAGES_FILE='./macOS/packages'
 
 xcode-select --install &>/dev/null
 
-echo "[+] Installing packages"
+echo "[+] Installing cask packages"
+brew tap caskroom/cask
+brew cask install xquartz
+
+echo "[+] Installing brew packages"
 while read -r package; do
   echo "Brewing formula ${package}"
   brew install "${package}"
