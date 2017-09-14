7ae4d5e0b8f140af991a0db60dd55351e6b800f0
brenj <brenj@mailbox.org>
2017-09-14 17:14:08 -0500

    Pause for commandline developer tools install

--- macOS/provision.sh
+++ macOS/provision.sh
@@ -5,6 +5,10 @@
 PACKAGES_FILE='./macOS/packages'
 
 xcode-select --install &>/dev/null
+read -n 1 -p "[+] Waiting for developer-tools install. Done (y/n)?: " input
+if [ "${input}" != 'y' ]; then
+  exit 1
+fi
 
 echo "[+] Installing Brew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
