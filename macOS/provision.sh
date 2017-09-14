86979f3722fa9eda763e2cfa4eee56be770e0488
brenj <brenj@mailbox.org>
2017-09-14 08:33:40 -0500

    Brew should not install with root

--- macOS/provision.sh
+++ macOS/provision.sh
@@ -2,11 +2,6 @@
 #
 # Provision a macOS machine.
 
-if [[ ${EUID} != 0 ]] ; then
-  echo "This script must be run as root"
-  exit 1
-fi
-
 PACKAGES_FILE='packages'
 
 xcode-select --install &>/dev/null
