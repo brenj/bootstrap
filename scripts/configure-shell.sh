5e2c56620ca0cb0e422dc0926ff35e367c0fdd7a
brenj <10823264+brenj@users.noreply.github.com>
2022-08-14 20:14:24 -0400

    Remove sym link to .tmux

--- scripts/configure-shell.sh
+++ scripts/configure-shell.sh
@@ -35,7 +35,6 @@ ln -sf dotfiles/.bash_profile .bash_profile
 ln -sf dotfiles/.gvimrc .gvimrc
 ln -sf dotfiles/.tern-config .tern-config
 ln -sf dotfiles/.tmux.conf .tmux.conf
-ln -sf dotfiles/.tmux .tmux
 ln -sf dotfiles/.vimrc .vimrc
 ln -sf dotfiles/.vim .vim
 ln -sf dotfiles/.eslintrc .eslintrc
